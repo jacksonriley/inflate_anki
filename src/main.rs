3db5c7a763e68b8dee59c8f1ef3b9ff6c428ec3f
Jackson Riley <jackson.riley@btinternet.com>
2024-01-31 21:19:02 +0000

    Add tone tagging - haven't bothered to implement actually adding the css though

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use clap::Parser;
-use sqlite::State;
+use rusqlite::params;
 use std::collections::HashMap;
 use std::fs::File;
 use std::path::{Path, PathBuf};
 use tempfile::tempdir;
 use zip::read::ZipArchive;
 use zip::write::ZipWriter;
+use zw_to_anki::dict::CEDict;
 
 const FIELD_SPLIT_CHAR: char = '\x1f';
 
@@ -38,8 +39,10 @@ fn main() -> anyhow::Result<()> {
         std::io::copy(&mut file, &mut outfile)?;
     }
 
+    let dict = CEDict::new();
+
     for collection in outfiles.iter().filter(|s| s.starts_with("collection")) {
-        convert_collection(&tmp_dir.path().join(collection))?;
+        convert_collection(&tmp_dir.path().join(collection), &dict)?;
     }
 
     let mut zip_writer = ZipWriter::new(File::create(args.out_file)?);
@@ -54,37 +57,35 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn convert_collection(path: &Path) -> anyhow::Result<()> {
-    let conn = sqlite::open(path)?;
+fn convert_collection(path: &Path, dict: &CEDict) -> anyhow::Result<()> {
+    let conn = rusqlite::Connection::open(path)?;
 
-    let query = "SELECT id, flds FROM notes;";
-    let mut statement = conn.prepare(query)?;
+    let mut stmt = conn.prepare("SELECT id, flds FROM notes")?;
+    let mut rows = stmt.query([])?;
     let mut new_flds: HashMap<i64, String> = HashMap::new();
-    while let Ok(State::Row) = statement.next() {
-        let id = statement.read::<i64, _>("id")?;
-        let flds = statement.read::<String, _>("flds")?;
+    while let Some(row) = rows.next()? {
+        let id = row.get(0)?;
+        let flds: String = row.get(1)?;
         // TODO: Don't convert every field, just the answer one
         new_flds.insert(
             id,
             flds.split(|b| b == FIELD_SPLIT_CHAR)
-                .map(plecoise)
+                .map(|s| plecoise(s, dict))
                 .collect::<Vec<String>>()
                 .join(&FIELD_SPLIT_CHAR.to_string()), // TODO: this to_string seems sad
         );
     }
-    let query = "UPDATE notes SET flds = :flds WHERE id = :id;";
+    let query = "UPDATE notes SET flds = ?1 WHERE id = ?2;";
     let mut statement = conn.prepare(query)?;
 
     for (id, flds) in new_flds.into_iter() {
-        statement.bind::<&[(_, sqlite::Value)]>(&[(":flds", flds.into()), (":id", id.into())])?;
-        while let State::Row = statement.next()? {}
-        statement.reset()?;
+        statement.execute(params![flds, id])?;
     }
     Ok(())
 }
 
 /// Replace the hanzi in a given text with Pleco links
-fn plecoise(text: &str) -> String {
+fn plecoise(text: &str, dict: &CEDict) -> String {
     let mut segments = text.chars().fold(Vec::new(), |mut acc, c| {
         let current_segment = acc.last_mut();
         let is_hz = cjk::is_simplified_chinese(&c.to_string());
@@ -117,10 +118,10 @@ fn plecoise(text: &str) -> String {
     // Make it idempotent, in a pretty gross manner...
     for i in 0..segments.len() {
         if let Some(Segment::NonHz(s2)) = segments.get(i + 1) {
-            if s2.starts_with("</a>") || s2.starts_with(r#"" style"#) {
+            if s2.starts_with("</a>") || s2.starts_with(r#"" style"#) || s2.starts_with("</span>") {
                 if let Some(s1) = segments.get_mut(i) {
                     if let Segment::Hz(inner) = s1 {
-                        // Assume that this means that s1 has already been wrapped in a pleco link
+                        // Assume that this means that s1 has already been inflated
                         // Therefore, turn it into a NonHz for idempotency
                         *s1 = Segment::NonHz(inner.clone());
                     }
@@ -131,7 +132,10 @@ fn plecoise(text: &str) -> String {
 
     segments.into_iter().map(|segment| match segment {
         Segment::NonHz(s) => s,
-        Segment::Hz(s) => format!(r#"<a href="plecoapi://x-callback-url/s?q={s}" style="text-decoration:none">{s}</a>"#)
+        Segment::Hz(s) => {
+            let colourised = dict.get(&s).into_iter().map(zw_to_anki::anki::Anki::to_colour_hanzi).collect::<Vec<_>>().join("");
+            format!(r#"<a href="plecoapi://x-callback-url/s?q={s}" style="text-decoration:none">{colourised}</a>"#)
+        }
     }).reduce(|s1, s2| format!("{s1}{s2}")).unwrap_or_else(String::new)
 }
 
@@ -146,10 +150,22 @@ mod tests {
     use super::*;
     #[test]
     fn test_plecoise() {
-        assert_eq!(plecoise("hello there"), "hello there".to_string());
-        assert_eq!(plecoise("hello there, 你好"), r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none">你好</a>"#.to_string());
-        assert_eq!(plecoise("hello there, 你好, how's it going, 你怎么样"), r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none">你好</a>, how's it going, <a href="plecoapi://x-callback-url/s?q=你怎么样" style="text-decoration:none">你怎么样</a>"#.to_string());
-        assert_eq!(plecoise(r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none">你好</a>"#), r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none">你好</a>"#.to_string());
-        assert_eq!(plecoise(r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none">你好</a>, how's it going, 你怎么样"#), r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none">你好</a>, how's it going, <a href="plecoapi://x-callback-url/s?q=你怎么样" style="text-decoration:none">你怎么样</a>"#.to_string());
+        let dict = CEDict::new();
+        assert_eq!(plecoise("hello there", &dict), "hello there".to_string());
+        assert_eq!(
+            plecoise("hello there, 你好", &dict),
+            r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none"><span class="tone3">你</span><span class="tone3">好</span></a>"#.to_string());
+        assert_eq!(
+            plecoise("hello there, 你好, how's it going, 你怎么样", &dict),
+            r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none"><span class="tone3">你</span><span class="tone3">好</span></a>, how's it going, <a href="plecoapi://x-callback-url/s?q=你怎么样" style="text-decoration:none"><span class="tone3">你</span><span class="tone3">怎</span><span class="tone5">么</span><span class="tone4">样</span></a>"#.to_string());
+        assert_eq!(
+            plecoise(r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none">你好</a>"#, &dict),
+            r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none">你好</a>"#.to_string());
+        assert_eq!(
+            plecoise(r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none"><span class="tone3">你</span><span class="tone3">好</span></a>"#, &dict),
+            r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none"><span class="tone3">你</span><span class="tone3">好</span></a>"#.to_string());
+        assert_eq!(
+            plecoise(r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none">你好</a>, how's it going, 你怎么样"#, &dict),
+            r#"hello there, <a href="plecoapi://x-callback-url/s?q=你好" style="text-decoration:none">你好</a>, how's it going, <a href="plecoapi://x-callback-url/s?q=你怎么样" style="text-decoration:none"><span class="tone3">你</span><span class="tone3">怎</span><span class="tone5">么</span><span class="tone4">样</span></a>"#.to_string());
     }
 }
